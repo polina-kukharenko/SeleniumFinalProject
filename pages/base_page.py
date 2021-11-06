6f9bbc7256a47ba70629c815b8710089a294dfa5
polinakukharenko <kukharenko.p@yandex.ru>
2021-11-07 00:57:59 +0300

    new test added; page object classes updated.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         return self.browser.get(self.url)
+
+    def is_element_present(self, search_method, search_string):
+        try:
+            self.browser.find_element(search_method, search_string)
+        except NoSuchElementException:
+            return False
+        return True
