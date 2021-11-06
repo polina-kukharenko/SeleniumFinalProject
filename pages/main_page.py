d53a492e9701b3a66bf4395b0015ee10c19b655e
polinakukharenko <kukharenko.p@yandex.ru>
2021-11-07 00:44:26 +0300

    first test modified; object page pattern added.

---
+++ pages/main_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+
+from .base_page import BasePage
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
